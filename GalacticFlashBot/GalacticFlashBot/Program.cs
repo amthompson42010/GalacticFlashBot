5ec3b2bcc1e647b47c8dd9716d42d8ec54c463f1
Alexander Mark Thompson <amthompson42010@gmail.com>
2016-08-03 23:21:03 -0500

    Made a simple twitch bot, will be adding to it soon.

---
+++ GalacticFlashBot/GalacticFlashBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticFlashBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IrcClient irc = new IrcClient("irc.twitch.tv", 6667, "galacticflashbot", "");
+            irc.joinRoom("galactic_flash");
+            while (true)
+            {
+                string message = irc.readMessage();
+                if (message.Contains("!hello"))
+                {
+                    irc.sendChatMessage("Hello there!");
+                }
+            }
+        }
+    }
+}
